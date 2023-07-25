4fc4d8c3c3164a46d046875bdd5a7f2b831656e5
YazeedSalahat <128978042+YazeedSalahat@users.noreply.github.com>
2023-07-25 13:52:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(() =>{
         let target = $("#curr2").val();
         let value = parseFloat($("#value").val());
         $.ajax({
-            url:"https://v6.exchangerate-api.com/v6/8bbfd5a1ec4e713a8448437c/pair/"+base+"/"+target+"/"+value,
+            url:"https://v6.exchangerate-api.com/v6/YourAPIKey/pair/"+base+"/"+target+"/"+value,
             type: "GET",
             success: (response) =>{
                 console.log(response);
